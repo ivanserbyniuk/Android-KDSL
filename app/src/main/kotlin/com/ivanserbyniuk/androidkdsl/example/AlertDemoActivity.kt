b1d5457d540e25ff87f0abea6cdd876404bb910e
ivanserbyniuk <ivan.serbyniuk@livestream.com>
2018-01-16 17:52:39 +0200

     Simple refactoring 2

--- app/src/main/kotlin/com/ivanserbyniuk/androidkdsl/example/AlertDemoActivity.kt
+++ app/src/main/kotlin/com/ivanserbyniuk/androidkdsl/example/AlertDemoActivity.kt
@@ -4,8 +4,6 @@ import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AppCompatActivity
-import android.view.LayoutInflater
-import android.widget.FrameLayout
 import com.ivanserbyniuk.androidkdsl.*
 
 class AlertDemoActivity : AppCompatActivity() {
@@ -46,8 +44,8 @@ class AlertDemoActivity : AppCompatActivity() {
 
     class AlertDialogFragment : DialogFragment() {
         override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            val view = LayoutInflater.from(activity).inflate(R.layout.view_info, FrameLayout(activity))
-            view?.findViewById(R.id.btnClose)?.setOnClickListener { dialog.dismiss() }
+            val view = context.inflate(R.layout.view_info)
+            view.findViewById(R.id.btnClose).setOnClickListener { dialog.dismiss() }
             //..and other listeners
             return alertDialog {
                 title(R.string.alert_title)
